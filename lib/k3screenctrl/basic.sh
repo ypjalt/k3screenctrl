da8a22a25ad80fd4d51a9555c1cec0d3f1b2efac
ypjalt <50544216@qq.com>
2017-07-09 20:56:29 +0800

    Update basic.sh

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-. /etc/os-release
+. /etc/openwrt_release
 
 PRODUCT_NAME_FULL=$(cat /etc/board.json | jsonfilter -e "@.model.name")
 PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
@@ -7,18 +7,11 @@ PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
 WAN_IFNAME=$(uci get network.wan.ifname)
 MAC_ADDR=$(ifconfig $WAN_IFNAME | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
 
-HW_VERSION_CACHE_FILE=/tmp/hw_version
-if [ -e "$HW_VERSION_CACHE_FILE" ]; then
-    HW_VERSION=$(cat $HW_VERSION_CACHE_FILE)
-else
-    HW_VERSION=$(nvram get hd_version)
-    echo $HW_VERSION > $HW_VERSION_CACHE_FILE
-fi
-
-FW_VERSION=${BUILD_ID:0:17}
+HW_VERSION=${DISTRIB_REVISION:0:6}
+FW_VERSION=${DISTRIB_ID}" "${DISTRIB_RELEASE}
 CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp)/1000))
 
-echo $PRODUCT_NAME
-echo $HW_VERSION" "$CPU_TEMP"C"
+echo $PRODUCT_NAME" "$CPU_TEMP
+echo $HW_VERSION
 echo $FW_VERSION
 echo $MAC_ADDR
