b6c5f4ae8715128e9b594bc3831eec20cfe3f74b
ypjalt <50544216@qq.com>
2017-07-15 19:13:08 +0800

    Update basic.sh

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -7,8 +7,8 @@ PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
 WAN_IFNAME=$(uci get network.wan.ifname)
 MAC_ADDR=$(ifconfig $WAN_IFNAME | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
 
-HW_VERSION=${DISTRIB_REVISION:0:6}
-FW_VERSION=${DISTRIB_ID}" "${DISTRIB_RELEASE}
+HW_VERSION=$(uname -r)
+FW_VERSION=${DISTRIB_ID}" "${DISTRIB_RELEASE}" "${DISTRIB_REVISION}
 CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp)/1000))
 
 echo $PRODUCT_NAME" "$CPU_TEMP
