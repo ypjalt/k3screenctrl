839c2eb6c6bcf551dd3e806e8db1c246332c34be
ypjalt <50544216@qq.com>
2017-09-07 14:08:08 +0800

    update

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -7,11 +7,17 @@ PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
 WAN_IFNAME=$(uci get network.wan.ifname)
 MAC_ADDR=$(ifconfig $WAN_IFNAME | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
 
+
+RUPTIME=$(awk '{print int($1/86400)"Days "int($1%86400/3600)"h "int(($1%3600)/60)"m"}' /proc/uptime)
+LAN_ADDR=`ifconfig br-lan |grep -w "inet addr"| awk '{print $2}'|awk -F':' '{print $2}' 2>/dev/null`
+[ -z "$LAN_ADDR" ] && LAN_ADDR=`uci get network.lan.ipaddr 2>/dev/null`
+[ -z "$LAN_ADDR" ] && LAN_ADDR="p.to"
+
 HW_VERSION=$(uname -r)
 FW_VERSION=${DISTRIB_ID}" "${DISTRIB_RELEASE}" "${DISTRIB_REVISION}
 CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp)/1000))
 
-echo $PRODUCT_NAME" "$CPU_TEMP
+echo $PRODUCT_NAME" "$CPU_TEMP"'C"
 echo $HW_VERSION
-echo $FW_VERSION
-echo $MAC_ADDR
+echo $LAN_ADDR
+echo $RUPTIME
